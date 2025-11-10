e6042f638f2eabb6c1a33a051ca2f20b894fb328
Yurii Sydoriak <yury.sydoriak@energyaspects.com>
2025-11-10 10:42:25 +0000

    Remove nitro prerender configuration
    Removed nitro prerender configuration from nuxt.config.ts.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  nitro: {
-    prerender: {
-      routes: ['/'],
-      crawlLinks: true,
-    },
-  },
   app: {
     head: {
       script: [
         {
           src: "https://web.cmp.usercentrics.eu/ui/loader.js",
           type: "application/javascript",
-        id: "usercentrics-cmp",
-        "data-ruleset-id": "y3gXuK6uG_sUwg",
-        async: true,
+          id: "usercentrics-cmp",
+          "data-ruleset-id": "y3gXuK6uG_sUwg",
+          async: true,
         },
       ],
     },
