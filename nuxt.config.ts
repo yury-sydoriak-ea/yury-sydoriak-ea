1a877f9c293ee1bdfba6930274df82e700b35b99
Yurii Sydoriak <yury.sydoriak@energyaspects.com>
2025-11-10 11:35:28 +0000

    Add Nitro prerender configuration to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: [],
+    },
+  },
   app: {
     head: {
       script: [
